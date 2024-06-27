b8bb5d14c476ecad0eca6f56c93f7ff14cc63fd5
Somjit Nag <somjitnag192@gmail.com>
2024-06-27 19:50:59 +0530

    practice

--- src/test/java/org/playground/JavaStreamsTest.java
+++ src/test/java/org/playground/JavaStreamsTest.java
@@ -5,9 +5,12 @@ import org.junit.jupiter.api.Test;
 
 import java.nio.file.Paths;
 import java.util.Comparator;
+import java.util.Currency;
 import java.util.List;
 import java.util.Random;
+import java.util.stream.Collectors;
 
+import static java.util.stream.Collectors.*;
 import static utils.Utils.loadEmployeesFromCSV;
 
 @FunctionalInterface
@@ -56,6 +59,50 @@ public class JavaStreamsTest {
 
         System.out.println(random20Numbers);
         System.out.println(secondHighest);
+    }
+
+
+    /*
+        flatten a list of list
+     */
+    @Test
+    void test2() {
+        List<String> inner1 = List.of("a", "b", "c");
+        List<String> inner2 = List.of("x", "y", "z");
+
+        List<List<String>> outer = List.of(inner1, inner2);
+
+        outer.stream()
+                .flatMap(List::stream)
+                .forEach(System.out::println);
+
+    }
+
+    /*
+        Group a list of transactions by currency and then sum the value of transactions in each currency:
+     */
+    @Test
+    void test4() {
+        record Txn(String currency, Integer amount) {
+        }
+
+        Txn t1 = new Txn("USD", 10);
+        Txn t2 = new Txn("USD", 10);
+        Txn t3 = new Txn("USD", 10);
+        Txn t4 = new Txn("EUR", 20);
+        Txn t5 = new Txn("EUR", 10);
+        Txn t6 = new Txn("EUR", 30);
+        Txn t7 = new Txn("GBP", 50);
+        Txn t8 = new Txn("INR", 1000);
+        Txn t9 = new Txn("YEN", 100);
+
+
+        List<Txn> txns = List.of(t1, t2, t3, t4, t5, t6, t7, t8, t9);
+
+        txns.stream()
+                .collect(groupingBy(Txn::currency, summingInt(Txn::amount)))
+                .forEach((currency, sum) -> System.out.printf("Sum for group: %s == %s\n", currency, sum));
+
     }
 
 
